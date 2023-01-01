d6fca931225fefe69df64d943bcab358d3ad4172
William Rogers <williamrogersdev@gmail.com>
2023-01-01 00:58:28 -0600

    finished the sign in page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,51 +1,60 @@
-// import React from 'react'
 import { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { toast } from "react-toastify";
 
 export default function SignIn() {
-  //hook for showing password
-
   const [showPassword, setShowPassword] = useState(false);
-
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
-
   const { email, password } = formData;
-
-  //onchagne function. gets all info we are getting in the form
-  //Whatever is typed is saved inside form data
-
-  function onChange(event) {
+  const navigate = useNavigate();
+  function onChange(e) {
     setFormData((prevState) => ({
       ...prevState,
-      [event.target.id]: event.target.value,
+      [e.target.id]: e.target.value,
     }));
   }
-
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("Bad user credentials");
+    }
+  }
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
       <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
         <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
           <img
-            src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1400&q=60"
-            alt="house"
+            src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
+            alt="key"
             className="w-full rounded-2xl"
           />
         </div>
-        <div className="w-full md-w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form onSubmit={onSubmit}>
             <input
               type="email"
               id="email"
               value={email}
               onChange={onChange}
               placeholder="Email address"
-              className=" mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
             />
             <div className="relative mb-6">
               <input
@@ -70,21 +79,20 @@ export default function SignIn() {
             </div>
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
               <p className="mb-6">
-                Don't have an account?
+                Don't have a account?
                 <Link
                   to="/sign-up"
                   className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
                 >
-                  {" "}
                   Register
                 </Link>
               </p>
               <p>
                 <Link
                   to="/forgot-password"
-                  className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out "
+                  className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out"
                 >
-                  Forgot Password?
+                  Forgot password?
                 </Link>
               </p>
             </div>
@@ -92,12 +100,12 @@ export default function SignIn() {
               className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
               type="submit"
             >
-              Sign In
+              Sign in
             </button>
-            <div className=" flex items-center my-4 before:border-t before:flex-1  before:border-gray-400 after:border-t after:flex-1  after:border-gray-400">
+            <div className="flex items-center  my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
               <p className="text-center font-semibold mx-4">OR</p>
             </div>
-            <OAuth/>
+            <OAuth />
           </form>
         </div>
       </div>
