8ae2614f76e3f2ee81ce27d7e4fbd4564cd4823f
William Rogers <williamrogersdev@gmail.com>
2023-01-09 22:40:31 -0600

    added edit and delete listing funcs

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { getAuth, updateProfile } from "firebase/auth";
 import {
   collection,
+  deleteDoc,
   doc,
   getDocs,
   orderBy,
@@ -84,6 +85,28 @@ export default function Profile() {
     }
     fetchUserListings();
   }, [auth.currentUser.uid]);
+
+
+async function onDelete(listingID) {
+
+  if (window.confirm('Are you sure you want to delete this listing?')) {
+    await deleteDoc(doc(db, "listings", listingID))
+    const updatedListings = listings.filter(
+      (listing) => listing.id !== listingID
+    )
+
+    setListings(updatedListings)
+    toast.success("Listing Has Been Deleted")
+  }
+
+}
+
+
+function onEdit(listingID) {
+  navigate(`/edit-listing/${listingID}`)
+}
+
+
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -164,6 +187,8 @@ export default function Profile() {
                   key={listing.id}
                   id={listing.id}
                   listing={listing.data}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
