40d5b2b24558cc3e252e22fc3ac47e66621a3cca
William Rogers <williamrogersdev@gmail.com>
2023-01-22 14:37:50 -0600

    fixing map issue

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -24,6 +24,7 @@ import { getAuth } from "firebase/auth";
 import Contact from "../components/Contact";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
+
 export default function Listing() {
   const auth = getAuth();
   const params = useParams();
@@ -150,24 +151,20 @@ export default function Listing() {
           )}
         </div>
         <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2">
-          <MapContainer
-            center={[listing.geolocation.lat, listing.geolocation.lng]}
-            zoom={13}
-            scrollWheelZoom={false}
-            style={{ height: "100%", width: "100%" }}
-          >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker
-              position={[listing.geolocation.lat, listing.geolocation.lng]}
-            >
-              <Popup>
-                {listing.address}
-              </Popup>
-            </Marker>
-          </MapContainer>
+          
+        <MapContainer center={listing.geolocation.lat, listing.geolocation.lng} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={listing.lat, listing.lng}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+
+
         </div>
       </div>
     </main>
