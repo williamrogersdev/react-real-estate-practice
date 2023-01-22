7116bfceb9d02c2f128b956d1f2bc90251d937bc
William Rogers <williamrogersdev@gmail.com>
2023-01-22 15:11:00 -0600

    made mistake had to move all from home page into slider component

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,41 @@
-import React from 'react'
-
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import { useState } from "react";
+import { useEffect } from "react";
+import Spinner from "../components/Spinner";
+import { db } from "../firebase";
 export default function Slider() {
+    const [listings, setListings] = useState(null);
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      async function fetchListings() {
+        const listingsRef = collection(db, "listings");
+        const q = query(listingsRef, orderBy("timestamp", "desc"), limit(5));
+        const querySnap = await getDocs(q);
+        let listings = [];
+  
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+  
+        setListings(listings);
+        setLoading(false);
+        console.log(listings);
+      }
+  
+      fetchListings();
+    }, []);
+  
+    if (loading) {
+      return <Spinner />;
+    }
+  
+    if (listings.length === 0) {
+      return <></>;
+    }
   return (
     <div>Slider</div>
   )
