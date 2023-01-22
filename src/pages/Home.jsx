699291b23c0bee3484d92aa919bcc0099d530766
William Rogers <williamrogersdev@gmail.com>
2023-01-22 15:07:19 -0600

    created listing and loading hooks for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,47 @@
 import { async } from "@firebase/util";
 import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import { useState } from "react";
 import { useEffect } from "react";
 import Slider from "../components/Slider";
+import Spinner from "../components/Spinner";
 import { db } from "../firebase";
 
 export default function Home() {
-
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
     async function fetchListings() {
-
-      const listingsRef = collection(db, "listings")
-      const q = query(listingsRef, orderBy('timestamp', 'desc'), limit(5))
-      const querySnap = await getDocs(q)
-      let listings = []
+      const listingsRef = collection(db, "listings");
+      const q = query(listingsRef, orderBy("timestamp", "desc"), limit(5));
+      const querySnap = await getDocs(q);
+      let listings = [];
 
       querySnap.forEach((doc) => {
         return listings.push({
           id: doc.id,
           data: doc.data(),
-        })
-      })
+        });
+      });
 
-
-
+      setListings(listings);
+      setLoading(false);
+      console.log(listings);
     }
 
-    fetchListings()
-  }, [])
+    fetchListings();
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (listings.length === 0) {
+    return <></>;
+  }
   return (
     <div>
-      <Slider/>
+      <Slider />
     </div>
-  )
+  );
 }
