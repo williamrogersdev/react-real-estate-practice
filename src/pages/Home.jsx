fada249389b90dea02db8fbb17b157cf923e72c5
William Rogers <williamrogersdev@gmail.com>
2023-01-22 14:55:41 -0600

    created useEffect func on page. getting each listing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,34 @@
+import { async } from "@firebase/util";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import { useEffect } from "react";
 import Slider from "../components/Slider";
+import { db } from "../firebase";
 
 export default function Home() {
+
+
+  useEffect(() => {
+
+    async function fetchListings() {
+
+      const listingsRef = collection(db, "listings")
+      const q = query(listingsRef, orderBy('timestamp', 'desc'), limit(5))
+      const querySnap = await getDocs(q)
+      let listings = []
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
+
+
+
+    }
+
+    fetchListings()
+  }, [])
   return (
     <div>
       <Slider/>
