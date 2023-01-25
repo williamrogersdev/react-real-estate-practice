4c36e5641cabb988529b41713ce0e57e7bb1ba4e
William Rogers <williamrogersdev@gmail.com>
2023-01-25 14:37:09 -0600

    creted fetchlistings func in offers page to get listings with offers on them

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,7 +1,33 @@
-// import React from 'react'
+import { collection, getDocs, limit, orderBy, query, where } from "firebase/firestore";
+import { useEffect } from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
 
 export default function Offers() {
-  return (
-    <div>Offers</div>
-  )
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchListings() {
+      try {
+        const listingRef = collection(db, "listings");
+        const q = query(
+          listingRef,
+          where("offer", "==", true),
+          orderBy("timestamp", "desc"),
+          limit(8)
+        );
+        const querySnap = await getDocs(q)
+
+        
+      } catch (error) {
+        toast.error("Could not fetch listings");
+      }
+    }
+
+    fetchListings();
+  }, []);
+
+  return <div>Offers</div>;
 }
